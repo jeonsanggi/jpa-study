780432f7a23de6292905f0f977f90e9839b61008
GM2202044 <sgjeon@gabia.com>
2022-05-24 17:24:40 +0900

    study : 객체지향 쿼리 언어1 - theory

---
+++ src/main/java/jpql/Team.java
@@ -0,0 +1,20 @@
+package jpql;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Team {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> member = new ArrayList<>();
+
+}
