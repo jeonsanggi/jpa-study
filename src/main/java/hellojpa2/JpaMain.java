bafb76e900b7505bf3a575d607e2732dda5b8f53
jeonsanggi <jsg2861@gmail.com>
2022-05-24 21:29:43 +0900

    study : 페치 조인1 - theory

--- src/main/java/hellojpa2/JpaMain.java
+++ src/main/java/hellojpa2/JpaMain.java
@@ -31,14 +31,48 @@ public class JpaMain {
 
              */
 
-            Member member = new Member();
-            member.setUsername("member1");
-            em.persist(member);
+            //페치 조인1
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
 
-            Team team = new Team();
-            team.setName("teamA");
-            team.getMembers().add(member);
-            em.persist(team);
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
+
+            em.flush();
+            em.clear();
+
+            /* 페치 조인을 사용하지 않으면
+             회원1, 팀A(SQL)
+             회원2, 팀A(1차 캐시)
+             회원3, 팀B(SQL)
+             총 3번의 쿼리를 사용하며 N + 1 문제가 발생
+             fetch join을 사용하면 1번의 쿼리를 발생하여 문제 해결
+             */
+            String query = "select m from Member m join fetch m.team";
+
+            List<Member> result = em.createQuery(query, Member.class)
+                            .getResultList();
+            for (Member member : result){
+                System.out.println("member = " + member.getUsername() + ", " + "teamName = " + member.getTeam().getName());
+            }
+
 
 
 
